43595bb30a717f6e703ce652a64e8146a5fe641e
Kayce Basques <kaycebasques@gmail.com>
2024-02-04 16:57:01 -0800

    Try to fix threading with events

--- src/blinky/blinky.py
+++ src/blinky/blinky.py
@@ -7,13 +7,16 @@ def blink():
     # The number maps to "GPIO 17" in the URL below, not the number in the circle.
     # https://www.raspberrypi.com/documentation/computers/raspberry-pi.html
     led = gpiozero.LED(17)
-    while True:
+    while not stop.is_set():
         led.on()
         time.sleep(1)
         led.off()
         time.sleep(1)
+    led.off()
 
+stop = threading.Event()
 thread = threading.Thread(target=blink)
 thread.start()
 input('Press Enter to stop...')
+stop.set()
 thread.join()
