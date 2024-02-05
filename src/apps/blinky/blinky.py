859e7276415c37b50c06b94b5ac9ec846b00ddfb
Kayce Basques <kaycebasques@gmail.com>
2024-02-04 16:30:27 -0800

    Add note about GPIO numbering

--- src/apps/blinky/blinky.py
+++ src/apps/blinky/blinky.py
@@ -1,6 +1,8 @@
 from gpiozero import LED
 from time import sleep
 
+# The number maps to "GPIO 17" in the URL below, not the number in the circle.
+# https://www.raspberrypi.com/documentation/computers/raspberry-pi.html
 led = LED(17)
 
 while True:
