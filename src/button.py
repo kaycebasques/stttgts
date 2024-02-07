68cf080a53287b3fe00347f72320190d6488fd99
Kayce Basques <kaycebasques@gmail.com>
2024-02-06 20:51:31 -0800

    Add logic for recording until button is released

--- src/button.py
+++ src/button.py
@@ -1,16 +1,21 @@
-# https://projects.raspberrypi.org/en/projects/physical-computing/5
-
+import subprocess
 import threading
 import time
 
 import gpiozero
 
+# https://projects.raspberrypi.org/en/projects/physical-computing/5
 button = gpiozero.Button(2)
+audio_file = 'user.flac'
 
 def record():
+    # start the audio recording
+    process = subprocess.Popen(['rec', audio_file, 'rate', '32k'])
+    # https://unix.stackexchange.com/a/57593/79351
     while not stop.is_set():
-        print('waiting...')
-    print('done')
+        # wait for it to end...
+        time.sleep(1)
+    process.terminate()
 
 while True:
     button.wait_for_press()
