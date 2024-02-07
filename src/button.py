3e5c25e7f81a3d8683b58f8fcc6eebc941b97bd8
Kayce Basques <kaycebasques@gmail.com>
2024-02-06 20:21:43 -0800

    Add threading logic to button app

--- src/button.py
+++ src/button.py
@@ -1,8 +1,22 @@
 # https://projects.raspberrypi.org/en/projects/physical-computing/5
+
+import threading
+import time
+
 import gpiozero
 
 button = gpiozero.Button(2)
-button.wait_for_press()
-print('the button is being pressed')
-button.wait_for_release()
-print('the button was released')
+
+def record():
+    while not stop.is_set():
+        print('waiting...')
+    print('done')
+
+while True:
+    button.wait_for_press()
+    stop = threading.Event()
+    thread = threading.Thread(target=record)
+    thread.start()
+    button.wait_for_release()
+    stop.set()
+    thread.join()
