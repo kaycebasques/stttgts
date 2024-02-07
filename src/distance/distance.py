4ab01643aa03f4b7aad8baaee42331f3dabe0959
Kayce Basques <kaycebasques@gmail.com>
2024-02-06 19:24:55 -0800

    Bump from 3s to 1s

--- src/distance/distance.py
+++ src/distance/distance.py
@@ -2,7 +2,9 @@ import time
 
 import gpiozero
 
+# https://gpiozero.readthedocs.io/en/stable/api_input.html#distancesensor-hc-sr04
+# https://projects.raspberrypi.org/en/projects/physical-computing/12
 ultrasonic = gpiozero.DistanceSensor(echo=17, trigger=4)
 while True:
     print(ultrasonic.distance)
-    time.sleep(3)
+    time.sleep(1)
