45f0089cab8ba2cbc658a03319b0dd57cbcd477e
Kayce Basques <kaycebasques@gmail.com>
2024-02-08 22:29:55 -0800

    Talking robot lives

--- src/button.py
+++ src/button.py
@@ -1,4 +1,5 @@
 import os
+import random
 import subprocess
 import threading
 import time
@@ -16,6 +17,7 @@ model = gemini.GenerativeModel('gemini-pro')
 # https://projects.raspberrypi.org/en/projects/physical-computing/5
 button = gpiozero.Button(2)
 audio_file = 'user.flac'
+led = gpiozero.RGBLED(16, 20, 21)
 
 # https://codelabs.developers.google.com/codelabs/cloud-speech-text-python3#3
 def speech_to_text():
@@ -40,24 +42,56 @@ def speech_to_text():
     # print(best_response)
 
 def record():
+    led.on()
+    led.color = (1, 0, 0)
     # start the audio recording
     process = subprocess.Popen(['rec', audio_file, 'rate', '48k'])
     # https://unix.stackexchange.com/a/57593/79351
-    while not stop.is_set():
+    while not stop_recording.is_set():
         # wait for it to end...
         time.sleep(1)
     process.terminate()
+    led.off()
+
+def thinky_blinky():
+    """blink like an octopus dreaming... https://youtu.be/0vKCLJZbytU"""
+    colors = [
+        (0, 0, 0),
+        (1, 0, 0),
+        (1, 1, 0),
+        (1, 0, 1),
+        (0, 1, 0),
+        (0, 1, 1),
+        (1, 1, 1)
+    ]
+    led.on()
+    while not stop_blinking.is_set():
+        led.color = random.choice(colors)
+        time.sleep(0.1)
+    led.off()
 
 while True:
     button.wait_for_press()
-    stop = threading.Event()
+    stop_recording = threading.Event()
     thread = threading.Thread(target=record)
     thread.start()
     button.wait_for_release()
-    stop.set()
+    stop_recording.set()
     thread.join()
-    time.sleep(1)
-    subprocess.run(['play', '-v', '3.0', audio_file])
+    stop_blinking = threading.Event()
+    thread2 = threading.Thread(target=thinky_blinky)
+    thread2.start()
+    # time.sleep(1)
+    # subprocess.run(['play', '-v', '3.0', audio_file])
     text = speech_to_text()
+    print(text)
     response = model.generate_content(text)
     print(response.text)
+    stop_blinking.set()
+    thread2.join()
+    led.on()
+    led.color = (0, 0.2, 1)
+    p = subprocess.Popen(['spd-say', '--wait', '--volume', '+100', f'"{response.text}"'])
+    while p.poll() is None:
+        time.sleep(1)
+    led.off()
